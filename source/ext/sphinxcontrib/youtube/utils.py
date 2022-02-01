cac78f583b6ba025b8587ab88bd25e041166fd7a
Egbert de Waal <e.de.w@live.nl>
2022-02-01 15:22:34 +0100

    Update plugin for YouTube Embeds to v1.1.0 https://github.com/sphinx-contrib/youtube

--- source/ext/sphinxcontrib/youtube/utils.py
+++ source/ext/sphinxcontrib/youtube/utils.py
@@ -29,6 +29,7 @@ def visit_video_node(self, node, platform_url):
     aspect = node["aspect"]
     width = node["width"]
     height = node["height"]
+    url_parameters = node["url_parameters"]
 
     if aspect is None:
         aspect = 16, 9
@@ -50,7 +51,7 @@ def visit_video_node(self, node, platform_url):
             "border": "0",
         }
         attrs = {
-            "src": "{}{}".format(platform_url,node['id']),
+            "src": "{}{}{}".format(platform_url,node['id'],url_parameters),
             "style": css(style),
         }
     else:
@@ -70,11 +71,13 @@ def visit_video_node(self, node, platform_url):
             "src":  "{}{}".format(platform_url,node['id']),
             "style": css(style),
         }
+    if node["align"] != None: div_style["text-align"] = node["align"]
     attrs["allowfullscreen"] = "true"
     div_attrs = {
         "CLASS": "video_wrapper",
         "style": css(div_style),
     }
+    if node["align"] != None: div_attrs["CLASS"] += " align-%s" % node["align"]
     self.body.append(self.starttag(node, "div", **div_attrs))
     self.body.append(self.starttag(node, "iframe", **attrs))
     self.body.append("</iframe></div>")
@@ -88,9 +91,9 @@ def visit_video_node_latex(self, node, platform, platform_url):
     macro = r"\sphinxcontrib%s" % platform
     if macro not in self.elements["preamble"]:
         self.elements["preamble"] += r"""
-        \newcommand{%s}[2]{\begin{quote}\begin{center}\fbox{\url{#1#2}}\end{center}\end{quote}}
+        \newcommand{%s}[3]{\begin{quote}\begin{center}\fbox{\url{#1#2#3}}\end{center}\end{quote}}
         """ % macro
-    self.body.append('%s{%s}{%s}\n' % (macro, platform_url, node['id']))
+    self.body.append('%s{%s}{%s}{%s}\n' % (macro, platform_url, node['id'], node['url_parameters']))
 
 
 class Video(Directive):
@@ -103,22 +106,37 @@ class Video(Directive):
         "width": directives.unchanged,
         "height": directives.unchanged,
         "aspect": directives.unchanged,
+        "align": directives.unchanged,
+        "url_parameters": directives.unchanged,
     }
 
     def run(self):
         if "aspect" in self.options:
             aspect = self.options.get("aspect")
-            m = re.match("(\d+):(\d+)", aspect)
+            m = re.match(r"(\d+):(\d+)", aspect)
             if m is None:
                 raise ValueError("invalid aspect ratio %r" % aspect)
             aspect = tuple(int(x) for x in m.groups())
         else:
             aspect = None
+        if "align" in self.options:
+            align = self.options.get("align")
+            if align not in ["left", "center", "right"]:
+                raise ValueError(
+                    "invalid alignment. "
+                    "choices are [\"left\", \"center\", \"right\"]"
+                )
+        else:
+            align = None
         width = get_size(self.options, "width")
         height = get_size(self.options, "height")
-        return [self._node(id=self.arguments[0], aspect=aspect, width=width, height=height)]
+        url_parameters = self.options.get("url_parameters", "")
+        return [self._node(id=self.arguments[0], aspect=aspect, width=width,
+                height=height, align=align, url_parameters=url_parameters)]
 
 
 def unsupported_visit_video(self, node, platform):
-    self.builder.warn('{}: unsupported output format (node skipped)'.format(platform))
+    self.builder.warn(
+        '{}: unsupported output format (node skipped)'.format(platform)
+    )
     raise nodes.SkipNode
